51b74f29b6f1142593d3d891d5e2d050001096b1
ayman <kirabotayman@gmail.com>
2026-02-09 13:29:32 +0300

    Update غادري.js

--- script/commands/developer/غادري.js
+++ script/commands/developer/غادري.js
@@ -1,39 +1,41 @@
-const axios = require("axios");
 const fs = require("fs-extra");
 const path = require("path");
+const axios = require("axios");
 
 module.exports.config = {
   name: "غادري",
-  version: "1.3.1",
+  version: "1.2.7",
   hasPermssion: 2,
-  credits: "ايمن",
+  credits: "Ayman",
   description: "مغادرة البوت للمجموعة",
-  commandCategory: "developer",
+  commandCategory: "المطور",
   usages: "غادري [ID]",
   cooldowns: 5
 };
 
-module.exports.run = async function({ api, event, args }) {
+module.exports.run = async ({ api, event, args }) => {
   const { threadID, messageID, senderID } = event;
 
-  if (!global.config.ADMINBOT.includes(senderID)) return;
+  // التحقق من المطور
+  const DEV_ID = "61577861540407"; 
+  if (String(senderID) !== DEV_ID) return;
 
   const targetID = args[0] || threadID;
-  const pathGif = path.join(__dirname, "cache", `leave.gif`);
+  const pathGif = path.join(__dirname, "cache", `bye.gif`);
 
   try {
-    const response = await axios.get("https://i.imgur.com/vHqY7bM.gif", { responseType: "arraybuffer" });
+    const response = await axios.get("https://media.giphy.com/media/kaBU6pgv0OsPHz2yxy/giphy.gif", { responseType: "arraybuffer" });
     fs.ensureDirSync(path.join(__dirname, "cache"));
     fs.writeFileSync(pathGif, Buffer.from(response.data));
 
     return api.sendMessage({
-      body: "⌬ ━━ 𝗞𝗜𝗥𝗔 ━━ ⌬\n\nوداعاً، تمت المغادرة بطلب من المطور.",
+      body: "⌬ ━━ 𝗞𝗜𝗥𝗔 ━━ ⌬\n\nنغادر الآن بكل هيبة.. وداعاً. 👑",
       attachment: fs.createReadStream(pathGif)
     }, targetID, () => {
       setTimeout(() => {
         api.removeUserFromGroup(api.getCurrentUserID(), targetID);
         if (fs.existsSync(pathGif)) fs.unlinkSync(pathGif);
-      }, 1000);
+      }, 1500);
     });
 
   } catch (e) {
